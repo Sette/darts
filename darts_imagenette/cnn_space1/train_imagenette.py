08242dee654b3f8ab950b379c7948d99d10e403d
Bruno Sette <bruno@pop-os.localdomain>
2021-02-04 16:05:28 -0300

    Update save model

--- darts_imagenette/cnn_space1/train_imagenette.py
+++ darts_imagenette/cnn_space1/train_imagenette.py
@@ -164,6 +164,8 @@ def main():
       'best_acc_top1': best_acc_top1,
       'optimizer' : optimizer.state_dict(),
       }, is_best, args.save)
+    
+    utils.save_all(model, 'weights_'+str(epoch)+'.pt'))
 
 
 def train(train_queue, model, criterion, optimizer):
